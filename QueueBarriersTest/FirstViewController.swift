cfcef0f65afe9979c43c82d02d7bb38450790263
Kuba Suder <jakub.suder@gmail.com>
2019-11-10 21:25:27 +0200

    created empty tab bar project

---
+++ QueueBarriersTest/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  QueueBarriersTest
+//
+//  Created by Kuba Suder on 10.11.2019.
+//  Copyright © 2019 Kuba Suder. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ QueueBarriersTest/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  QueueBarriersTest
+//
+//  Created by Kuba Suder on 10.11.2019.
+//  Copyright © 2019 Kuba Suder. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
